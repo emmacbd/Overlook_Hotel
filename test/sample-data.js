acf910baac97d800b6da94292154332a01210eb5
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-04-20 20:27:24 -0500

    Add sample data

---
+++ test/sample-data.js
@@ -0,0 +1,88 @@
+
+let sampleCustomers = [
+{
+id: 1,
+name: "Leatha Ullrich"
+},
+{
+id: 2,
+name: "Rocio Schuster"
+},
+{
+id: 9,
+name: "Faustino Quitzon"
+},
+]
+
+let sampleRooms = [
+{
+number: 2,
+roomType: "suite",
+bidet: false,
+bedSize: "full",
+numBeds: 2,
+costPerNight: 477.38
+},
+{
+number: 7,
+roomType: "single room",
+bidet: false,
+bedSize: "queen",
+numBeds: 2,
+costPerNight: 231.46
+},
+{
+number: 12,
+roomType: "single room",
+bidet: false,
+bedSize: "twin",
+numBeds: 2,
+costPerNight: 172.09
+},
+{
+number: 15,
+roomType: "residential suite",
+bidet: false,
+bedSize: "full",
+numBeds: 1,
+costPerNight: 294.56
+},
+{
+number: 23,
+roomType: "residential suite",
+bidet: false,
+bedSize: "queen",
+numBeds: 2,
+costPerNight: 176.36
+},
+]
+
+let sampleBookings = [
+{
+id: "5fwrgu4i7k55hl6sz",
+userID: 9,
+date: "2022/04/22",
+roomNumber: 15
+},
+{
+id: "5fwrgu4i7k55hl6t6",
+userID: 13,
+date: "2022/01/10",
+roomNumber: 12
+},
+{
+id: "5fwrgu4i7k55hl6t8",
+userID: 1,
+date: "2022/02/05",
+roomNumber: 12
+},
+{
+id: "5fwrgu4i7k55hl6vu",
+userID: 9,
+date: "2022/01/16",
+roomNumber: 23
+},
+]
+
+export {
+  sampleCustomers, sampleRooms, sampleBookings};
