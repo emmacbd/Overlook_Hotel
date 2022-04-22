b92695a33643f228bcd5424dedf2759640596d5f
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-04-22 11:40:17 -0500

    Add get booking method to Hotel class

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -1,6 +1,7 @@
 //rooms, bookings, available rooms, unavailable rooms, type of room
 
-//need book room method,
+//need book room method, which would first call method that finds available rooms?
+
 //need methods to find available rooms/determine unavailable
 
 //filter room by type
@@ -8,6 +9,23 @@
 //
 
 class Hotel {
+  constructor(hotelRooms, hotelBookings) {
+    this.rooms = hotelRooms;
+    this.bookings = hotelBookings;
+    this.availableRooms;
+  }
+
+//SOMEHOW GET AN IF STATEMENT IF NO RESULTS FOUND ??? WTF
+getBooking(bookingInfo){
+   return this.rooms.find(room =>
+    room.number === bookingInfo.roomNumber)
+}
+
+///check bookings array and compare with input date of user wtf how ok
+//dayjs
+//
+
+//filter room by type method dear god - param of roomtype, filter rooms array by roomtype but also will need check available rooms before returned filtered room by type FUCCK
 
 
 }
