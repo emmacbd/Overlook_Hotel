725e48d6cec96b18da74abc52bbcbdf4285275b0
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-04-22 19:10:39 -0500

    Add room selection options and filter box  to HTML

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,20 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
+import Hotel from '../src/classes/Hotel'// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './src/images/overlook.jpeg'
+
+//QUERY SELECTORS
+const customerDashboard = document.querySelector(".customer-dashboard");
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+
+//FUNCTIONS
+const displayCustomerBookings = (bookingData) => {
+  
+}
+//need to get a user
+//what about with login fuck idk
+//get users bookings
+//display previous and upcoming bookings
